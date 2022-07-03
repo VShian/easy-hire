1919a5f0f5a7851c1f1dcd02fc4bd40cf82c6919
shree397 <shree.harshadk@gmail.com>
2022-07-03 13:03:36 +0530

    updated text

--- src/components/Homepage/About.js
+++ src/components/Homepage/About.js
@@ -1,169 +1,117 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-  // const stats = [{
-  //   label: 'Companies have access to student leaderboard',
-  //   value: '50+',
-  //   description: 'We’ve helped build over 400 amazing projects.'
-  // },
-  // {
-  //   label: 'Companies have access to student leaderboard',
-  //   value: '50+',
-  //   description: 'We’ve helped build over 400 amazing projects.'
-  // },
-  // {
-  //   label: 'Companies have access to student leaderboard',
-  //   value: '50+',
-  //   description: 'We’ve helped build over 400 amazing projects.'
-  // },
-  // {
-  //   label: 'Companies have access to student leaderboard',
-  //   value: '50+',
-  //   description: 'We’ve helped build over 400 amazing projects.'
-  // }]
-  return (
-    <section className='min-h-[100vh] snap-start'>
-      <div class="flex flex-col justify-center items-center gap-14 py-16 bg-[#4f4f4f] h-full">
-        <div
-          class="flex flex-col justify-start items-center  gap-16 py-24 bg-[#4f4f4f]"
-        >
-          <div
-            class="flex flex-col justify-start items-start  max-w-[1280px] relative gap-3 px-8"
-          >
-            <p class=" text-base font-semibold text-left text-[#7d84c8]">
-              About us
-            </p>
-            <div class="flex justify-start items-start self-stretch flex-wrap gap-16">
-              <div
-                class="flex flex-col justify-start items-start  relative gap-10"
-              >
-                <p
-                  class="self-stretch text-5xl font-semibold text-left text-white"
-                >
-                  We do things differently...
-                </p>
-              </div>
-              <div class="flex flex-col justify-start items-start flex-grow relative pt-3">
-                <p class="self-stretch  w-96 text-xl text-left text-[#f2f2f2]">
-                  Learn more about the company and our success
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div
-          class="flex flex-col justify-start items-center  max-w-[1440px] overflow-hidden gap-16 pb-24 bg-[#4f4f4f]"
-        >
-          <div
-            class="flex flex-col justify-start items-start gap-16 px-8"
-          >
-            <div class="flex justify-start items-start self-stretch  gap-8 flex-wrap">
-              <div class="flex flex-col justify-start items-start gap-5">
-                <div
-                  class="flex flex-col justify-start items-start self-stretch  relative gap-3"
-                >
-                  <p
-                    class="self-stretch  w-[280px] text-6xl font-semibold text-left text-[#7d84c8]"
-                  >
-                    50+
-                  </p>
-                  <div
-                    class="flex flex-col justify-start items-start self-stretch  relative gap-2"
-                  >
-                    <p
-                      class="self-stretch  w-[280px] text-lg font-medium text-left text-white"
-                    >
-                      Companies have access to student leaderboard
-                    </p>
-                    <p
-                      class="self-stretch  w-[280px] text-base text-left text-[#f2f2f2]"
-                    >
-                      We’ve helped build over 400 amazing projects.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div class="flex flex-col justify-start items-start gap-5">
-                <div
-                  class="flex flex-col justify-start items-start self-stretch  relative gap-3"
-                >
-                  <p
-                    class="self-stretch  w-[280px] text-6xl font-semibold text-left text-[#7d84c8]"
-                  >
-                    600%
-                  </p>
-                  <div
-                    class="flex flex-col justify-start items-start self-stretch  relative gap-2"
-                  >
-                    <p
-                      class="self-stretch  w-[280px] text-lg font-medium text-left text-white"
-                    >
-                      Return on investment
-                    </p>
-                    <p
-                      class="self-stretch  w-[280px] text-base text-left text-[#f2f2f2]"
-                    >
-                      Our customers have reported an average of ~600% ROI.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div class="flex flex-col justify-start items-start gap-5">
-                <div
-                  class="flex flex-col justify-start items-start self-stretch  relative gap-3"
-                >
-                  <p
-                    class="self-stretch  w-[280px] text-6xl font-semibold text-left text-[#7d84c8]"
-                  >
-                    10k
-                  </p>
-                  <div
-                    class="flex flex-col justify-start items-start self-stretch  relative gap-2"
-                  >
-                    <p
-                      class="self-stretch  w-[280px] text-lg font-medium text-left text-white"
-                    >
-                      Global downloads
-                    </p>
-                    <p
-                      class="self-stretch  w-[280px] text-base text-left text-[#f2f2f2]"
-                    >
-                      Our free UI kit has been downloaded over 100k times.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div class="flex flex-col justify-start items-start gap-5">
-                <div
-                  class="flex flex-col justify-start items-start self-stretch  relative gap-3"
-                >
-                  <p
-                    class="self-stretch  w-[280px] text-6xl font-semibold text-left text-[#7d84c8]"
-                  >
-                    200+
-                  </p>
-                  <div
-                    class="flex flex-col justify-start items-start self-stretch  relative gap-2"
-                  >
-                    <p
-                      class="self-stretch  w-[280px] text-lg font-medium text-left text-white"
-                    >
-                      5-star reviews
-                    </p>
-                    <p
-                      class="self-stretch  w-[280px] text-base text-left text-[#f2f2f2]"
-                    >
-                      We’re proud of our 5-star rating with over 200 reviews.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
-}
+	// const stats = [{
+	//   label: 'Companies have access to student leaderboard',
+	//   value: '50+',
+	//   description: 'We’ve helped build over 400 amazing projects.'
+	// },
+	// {
+	//   label: 'Companies have access to student leaderboard',
+	//   value: '50+',
+	//   description: 'We’ve helped build over 400 amazing projects.'
+	// },
+	// {
+	//   label: 'Companies have access to student leaderboard',
+	//   value: '50+',
+	//   description: 'We’ve helped build over 400 amazing projects.'
+	// },
+	// {
+	//   label: 'Companies have access to student leaderboard',
+	//   value: '50+',
+	//   description: 'We’ve helped build over 400 amazing projects.'
+	// }]
+	return (
+		<section className="min-h-[100vh] snap-start">
+			<div class="flex flex-col justify-center items-center gap-14 py-16 bg-[#4f4f4f] h-full">
+				<div class="flex flex-col justify-start items-center  gap-16 py-24 bg-[#4f4f4f]">
+					<div class="flex flex-col justify-start items-start  max-w-[1280px] relative gap-3 px-8">
+						<p class=" text-base font-semibold text-left text-[#7d84c8]">
+							About us
+						</p>
+						<div class="flex justify-start items-start self-stretch flex-wrap gap-16">
+							<div class="flex flex-col justify-start items-start  relative gap-10">
+								<p class="self-stretch text-5xl font-semibold text-left text-white">
+									We do things differently...
+								</p>
+							</div>
+							<div class="flex flex-col justify-start items-start flex-grow relative pt-3">
+								<p class="self-stretch  w-96 text-xl text-left text-[#f2f2f2]">
+									Learn more about the company and stats.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="flex flex-col justify-start items-center  max-w-[1440px] overflow-hidden gap-16 pb-24 bg-[#4f4f4f]">
+					<div class="flex flex-col justify-start items-start gap-16 px-8">
+						<div class="flex justify-start items-start self-stretch  gap-8 flex-wrap">
+							<div class="flex flex-col justify-start items-start gap-5">
+								<div class="flex flex-col justify-start items-start self-stretch  relative gap-3">
+									<p class="self-stretch  w-[280px] text-6xl font-semibold text-left text-[#7d84c8]">
+										300+
+									</p>
+									<div class="flex flex-col justify-start items-start self-stretch  relative gap-2">
+										<p class="self-stretch  w-[280px] text-lg font-medium text-left text-white">
+											videos of learning
+										</p>
+										<p class="self-stretch  w-[280px] text-base text-left text-[#f2f2f2]">
+											Across all topics by experts.
+										</p>
+									</div>
+								</div>
+							</div>
+							<div class="flex flex-col justify-start items-start gap-5">
+								<div class="flex flex-col justify-start items-start self-stretch  relative gap-3">
+									<p class="self-stretch  w-[280px] text-6xl font-semibold text-left text-[#7d84c8]">
+										150+
+									</p>
+									<div class="flex flex-col justify-start items-start self-stretch  relative gap-2">
+										<p class="self-stretch  w-[280px] text-lg font-medium text-left text-white">
+											Hours of learning
+										</p>
+										<p class="self-stretch  w-[280px] text-base text-left text-[#f2f2f2]">
+											Across all specific topics company wise
+										</p>
+									</div>
+								</div>
+							</div>
+							<div class="flex flex-col justify-start items-start gap-5">
+								<div class="flex flex-col justify-start items-start self-stretch  relative gap-3">
+									<p class="self-stretch  w-[280px] text-6xl font-semibold text-left text-[#7d84c8]">
+										1500+
+									</p>
+									<div class="flex flex-col justify-start items-start self-stretch  relative gap-2">
+										<p class="self-stretch  w-[280px] text-lg font-medium text-left text-white">
+											Example questions
+										</p>
+										<p class="self-stretch  w-[280px] text-base text-left text-[#f2f2f2]">
+											Explained in both generic way and shortcut method.
+										</p>
+									</div>
+								</div>
+							</div>
+							<div class="flex flex-col justify-start items-start gap-5">
+								<div class="flex flex-col justify-start items-start self-stretch  relative gap-3">
+									<p class="self-stretch  w-[280px] text-6xl font-semibold text-left text-[#7d84c8]">
+										30000+
+									</p>
+									<div class="flex flex-col justify-start items-start self-stretch  relative gap-2">
+										<p class="self-stretch  w-[280px] text-lg font-medium text-left text-white">
+											Test questions
+										</p>
+										<p class="self-stretch  w-[280px] text-base text-left text-[#f2f2f2]">
+											Our data base is built on top of real data.
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
 
-export default About
+export default About;
--- src/components/Homepage/Mission.js
+++ src/components/Homepage/Mission.js
@@ -1,47 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const Mission = () => {
-  return (
-    <section className='min-h-[100vh] h-full snap-start'>
-      <div class="bg-purple h-full flex justify-center items-center">
-        <div class="flex flex-col justify-start items-center gap-16 py-24 bg-purple">
-          <div
-            class="flex flex-col justify-start items-center gap-8 px-8 max-w-[1280px]"
-          >
-            <div
-              class="flex flex-col justify-start items-center self-stretch  gap-12"
-            >
-              <div
-                class="flex flex-col justify-start items-center   relative gap-6"
-              >
-                <div
-                  class="flex flex-col justify-start items-start self-stretch  relative gap-3"
-                >
-                  <p
-                    class="self-stretch   text-base font-semibold text-center text-white"
-                  >
-                    Why us
-                  </p>
-                  <p
-                    class="self-stretch   text-5xl font-semibold text-center text-[#f2f2f2]"
-                  >
-                    Our mission is to personalized experience for every type of learners.
-                  </p>
-                </div>
-                <p class=" text-xl text-center text-[#f2f2f2]">
-                  Our products are interconnected to give a seamless personalized learning experience for
-                  all learners. The learning app has the contents prepared by best teachers which helps grow
-                  the interview skills required by candidates and the test platform gives personalized
-                  experience for every type of learners based on the scores they obtain and the placement
-                  portal strives to make their dream job just not a dream.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
-}
+	return (
+		<section className="min-h-[100vh] h-full snap-start">
+			<div class="bg-purple h-full flex justify-center items-center">
+				<div class="flex flex-col justify-start items-center gap-16 py-24 bg-purple">
+					<div class="flex flex-col justify-start items-center gap-8 px-8 max-w-[1280px]">
+						<div class="flex flex-col justify-start items-center self-stretch  gap-12">
+							<div class="flex flex-col justify-start items-center   relative gap-6">
+								<div class="flex flex-col justify-start items-start self-stretch  relative gap-3">
+									<p class="self-stretch   text-base font-semibold text-center text-white">
+										Why us
+									</p>
+									<p class="self-stretch   text-5xl font-semibold text-center text-[#f2f2f2]">
+										Our mission is to personalize experience for every type of
+										learners.
+									</p>
+								</div>
+								<p class=" text-xl text-center text-[#f2f2f2]">
+									Our products are interconnected to give a seamless
+									personalized learning experience for all learners. The
+									learning app has the contents prepared by best teachers which
+									helps grow the interview skills required by candidates and the
+									test platform gives personalized experience for every type of
+									learners based on the scores they obtain and the placement
+									portal strives to make their dream job just not a dream.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
 
-export default Mission
+export default Mission;
